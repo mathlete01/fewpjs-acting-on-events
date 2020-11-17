5d72ab70701a96be23e2fe92a94743f09bdf2533
mathlete01 <msallin@gmail.com>
2020-11-17 08:27:57 -0800

    commit tree f9fdb2f930d788f6759801c69ed871c1c6692562

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+// function moveDodgerLeft() {
+//     let newPosition = dodger.style.left.replace("px", "");
+//     console.log(`newPosition = ${newPosition}`)
+//     let position = parseInt(newPosition, 10);
+  
+//     if (position > 0) {
+//       dodger.style.left = `${position - 10}px`;
+//     }
+//   }
+
+function moveDodger(dir) {
+    let newPosition = dodger.style.left.replace("px", "");
+    console.log(`newPosition = ${newPosition}`)
+    let position = parseInt(newPosition, 10);
+  
+    if (position > 0) {
+        if (dir == "left") {
+      dodger.style.left = `${position - 10}px`;
+        } 
+    }
+    if (position < (355)) {
+        if (dir == "right") {
+      dodger.style.left = `${position + 10}px`;
+        } 
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodger("left");
+    }else if (e.key === "ArrowRight") {
+        moveDodger("right");
+      }
+  });
